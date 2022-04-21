7d1cafb7afbad34018ddca59311d8d0c6eebc27f
Michael C. Tiberio <github@tiberio.org>
2022-04-21 11:00:20 -0700

    Adding more tests

--- GenesisPalindrome.Tests/GenesisPalindromeTests.cs
+++ GenesisPalindrome.Tests/GenesisPalindromeTests.cs
@@ -6,9 +6,27 @@ namespace GenesisPalindrome.Tests;
 public class PalindromeTests
 {
     [Theory]
+    [InlineData("")]
+    [InlineData("a")]
     [InlineData("aba")]
-    [InlineData("Aba")]
+    [InlineData("abba")]
+    [InlineData("abcba")]
     public void ShouldBePalindrome(string word)
+    {
+        Palindrome palindrome = new ();
+
+        bool result = palindrome.IsPalindrome(word);
+
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData("Aba")]
+    [InlineData("aBba")]
+    [InlineData("abbc")]
+    [InlineData("abca")]
+    [InlineData("abcca")]
+    public void ShouldNotBePalindrome(string word)
     {
         Palindrome palindrome = new ();
 
